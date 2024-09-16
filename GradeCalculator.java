19e5952de61eacd4d220f0cdc072ae5f1a7466b1
LHNT2k7 <ltri9573@gmail.com>
2024-09-16 10:15:49 -0700

    dfgh

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,31 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
-    // insert code here
+    String Course = "AP Calculus AB";
+    System.out.println("Course name: " + Course);
+    int Avetime = 145
+    int HW1 = 89;
+    int HW2 = 95;
+    int HW3 = 100;
+    int HW4 = 85;
+    double q1 = 93.5;
+    double q2 = 80.0;
+    double f1 = 88.75;
+    System.out.println("Average time spent in a week for this course in minutes: " + Avetime);
+    System.out.println("homework grades for this course:");
+    System.out.println(HW1);
+    System.out.println(HW2);
+    System.out.println(HW3);
+    System.out.println(HW4);
+    System.out.println("quiz grades for this course: ");
+    System.out.println(q1);
+    System.out.println(q2);
+    System.out.println("final exam grade for this course: ");
+    System.out.println(f1);
+    int Avetime1 = 145 / 60;
+    double Avetime2 = 145 % 60;
+
+
+
   }
 }
